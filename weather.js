9ba20ee672458ea87ab77bb7c969aadb3280e5a2
RashRagna <rashragna@gmail.com>
2026-03-20 10:51:35 -0500

    added HDC LatLong to weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
-const latitude = 41.8781;  // Replace with YOUR city's latitude
-const longitude = -87.6298; // Replace with YOUR city's longitude
+const latitude = 33.42883769999043;  // Replace with YOUR city's latitude
+const longitude = --86.7287087342889; // Replace with YOUR city's longitude
+
 
 // The API URL asking for 7 days of daily high/low temperatures in Fahrenheit
 const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=auto`;
